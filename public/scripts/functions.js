95e3718de1c71c61068da0fcaf84c6e59f77ee58
Austin Corley <arcorley528@gmail.com>
2017-08-11 15:11:28 -0700

    Update functions.js

--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -1,3 +1,6 @@
+//var path = "http://flip3.engr.oregonstate.edu:9501/";
+var path = "http://flip3.engr.oregonstate.edu:9477/";
+
 function login(){
 	var user_nm = document.getElementById("username").value;
 	var password = document.getElementById("password").value;
@@ -5,7 +8,7 @@ function login(){
 	var formData = {user_nm: user_nm, password: password};
 
 	var req = new XMLHttpRequest();
-	req.open('POST', 'http://flip3.engr.oregonstate.edu:9477/authenticate', true); //this part authenticates the username and password with what's in the database
+	req.open('POST', path + 'authenticate', true); //this part authenticates the username and password with what's in the database
 	req.setRequestHeader("Content-Type", "application/json");
 
 	formData = JSON.stringify(formData);
@@ -13,7 +16,7 @@ function login(){
 
 	req.addEventListener('load', function(){ //listen for a response
 			if(req.status == 200){
-				window.location = "http://flip3.engr.oregonstate.edu:9477/landing"; //this is the line that's executed upon successful login. for us, that will be the map
+				window.location = path + "landing"; //this is the line that's executed upon successful login. for us, that will be the map
 			}
 			else{
 				alert("The username/password combination supplied was incorrect."); 
@@ -34,7 +37,7 @@ function addUser(){
 		var formData = {user_nm: user_nm, password: password, user_email: user_email};
 
 		var req = new XMLHttpRequest();
-		req.open('POST', 'http://flip3.engr.oregonstate.edu:9477/add_user', true);
+		req.open('POST', path + 'add_user', true);
 		req.setRequestHeader("Content-Type", "application/json");
 
 		formData = JSON.stringify(formData);
@@ -43,7 +46,7 @@ function addUser(){
 		req.addEventListener('load', function(){
 			if(req.status == 200){
 				alert("Account created successfully!");
-				window.location = "http://flip3.engr.oregonstate.edu:9477/preferences";
+				window.location = path + "preferences";
 			}
 			else{
 				alert("There was an error creating your account. Please try again later.");
